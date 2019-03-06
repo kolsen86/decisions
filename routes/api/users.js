c769aa2c5badf7bc815eb43ae41bbd70fd82bd49
Kenneth Olsen <olsenkenneth1986@gmail.com>
2019-03-05 20:49:23 -0500

    Fixed error with token authorization

--- routes/api/users.js
+++ routes/api/users.js
@@ -74,13 +74,12 @@ router.post("/login", (req, res) => {
               // User Is Matched
               const payload = { id: user.id, name: user.name, avatar: user.avatar } // Create JWT Payload
               // Sign Token
-                jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, () => {
-                    (err, token) => {
+                jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                    
                         res.json({
                             success: true,
                             token: "Bearer " + token
                         });
-                    }
                 });
             } else {
                 return res.status(400).json({password: "Password Incorrect"});

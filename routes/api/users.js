f5aed6995d26ba0037aec353dd03e3e18a967f8d
Kenneth Olsen <olsenkenneth1986@gmail.com>
2019-03-06 13:57:22 -0500

    Added Validator module and set it up

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys.js");
 const passport = require("passport");
 
+// Load input Validation
+const validateRegisterInput = require("../../validation/register");
+
 // Load User Model
 
 const User = require("../models/User");
@@ -21,6 +24,13 @@ router.get("/test", (req, res) => res.json({msg: "Users Works"}));
 // @ access Public
 
 router.post("/register", (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    // Check Validation
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
     User.findOne({ email: req.body.email })
     .then(user => {
         if(user) {

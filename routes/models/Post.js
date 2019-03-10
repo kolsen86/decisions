0be9a88cb0d695bf4deb4f132b7723dba974dcc6
Kenneth Olsen <olsenkenneth1986@gmail.com>
2019-03-09 22:11:24 -0500

    Added model for user posts

--- routes/models/Post.js
+++ routes/models/Post.js
@@ -11,4 +11,46 @@ const PostSchema = new Schema({
         type: String,
         required: true
     },
+    name: {
+        type: String
+    },
+    avatar: {
+        type: String
+    },
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: "users"
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: "users"
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            avatar: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
 });
+
+module.exports = Post = mongoose.model("post", PostSchema);

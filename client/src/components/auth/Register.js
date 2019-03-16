f3baa6e787697242ee2a066ff9676960c7aa4c63
Kenneth Olsen <olsenkenneth1986@gmail.com>
2019-03-16 19:23:41 -0400

    Added redux reducers and actions to register and login sections

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
-import axios from "axios";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
 import classnames from "classnames";
+import { connect } from "react-redux";
+import { registerUser } from "../../actions/authActions"; 
 
  class Register extends Component {
   constructor() {
@@ -17,6 +20,12 @@ import classnames from "classnames";
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.errors) {
+      this.setState({errors: nextProps.errors});
+    }
+  }
+
   onSubmit(e) {
     e.preventDefault();
 
@@ -27,9 +36,9 @@ import classnames from "classnames";
       password2: this.state.password2
     };
 
-    axios.post("/api/users/register", newUser)
-    .then(res => console.log(res.data))
-    .catch(err => this.setState({errors: err.response.data}));
+    this.props.registerUser(newUser, this.props.history);
+
+
   }
 
   onChange(e) {
@@ -39,6 +48,7 @@ import classnames from "classnames";
     render() {
 
       const {errors} = this.state;
+
         return (
           <div className="register">
           <div className="container">
@@ -113,4 +123,15 @@ import classnames from "classnames";
         }
       }
 
-export default Register;
+      Register.propTypes = {
+        registerUser: PropTypes.func.isRequired,
+        auth: PropTypes.object.isRequired,
+        errors: PropTypes.object.isRequired
+      }
+
+      const mapStateToProps = (state) => ({
+        auth: state.auth,
+        errors: state.errors
+      });
+
+export default connect(mapStateToProps, { registerUser })(withRouter(Register));
